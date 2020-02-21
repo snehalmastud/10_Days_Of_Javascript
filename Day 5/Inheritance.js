445580b33ef7f5d850a8cc5285a6aaab3b9c4148
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-21 21:32:46 +0530

    HackerRank's Javascript Challenge!

---
+++ Day 5/Inheritance.js
@@ -0,0 +1,15 @@
+
+Rectangle.prototype.area = function() {
+        return(this.w*this.h);
+    };
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+   
+    class Square extends Rectangle {
+        constructor(s) {
+            super(s);
+            this.h = s;
+            this.w = s;
+        }
+    };
